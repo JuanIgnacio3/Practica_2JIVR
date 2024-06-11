0bf5c1c069f8890bd9a50dcacafb04a858ebf050
Ignacio <Ignacio@DESKTOP-108IMK1>
2024-06-11 12:57:37 -0600

    Finalizacion practica 2

---
+++ src/main/java/com/practica_2JIVR/Practica2JivrApplication.java
@@ -0,0 +1,13 @@
+package com.practica_2JIVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2JivrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2JivrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica_2JIVR/Practica2JivrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_2JIVR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2JivrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
